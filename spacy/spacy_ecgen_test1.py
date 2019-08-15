013b7b5dc9babe642d4b0b5777fe85bb69bfc5cf
Amelia Woodward <ameliawd@stanford.edu>
2019-08-15 00:29:02 -0700

    Finished initial parser and tokenizer.

--- spacy/spacy_ecgen_test1.py
+++ spacy/spacy_ecgen_test1.py
@@ -7,45 +7,44 @@ import os
 import xml_to_text_p3 as parser
 
 
-def build_tokenizer(nlp, report_dir):
+def build_tokenizable_data(nlp, report_dir):
 	"""
-	Creates a tokenizer from the main bodies of text in .xml files passed in
+	Creates a list of strings from the main bodies of text in .xml files passed in
+	docs - a list of strings of the main body of reports
+
 	"""
-	#tokenizer = Tokenizer(nlp.vocab)
-	#print(tokenizer)
 
 	#Loads data from docs into a texts string:
 	docs = []
 	for doc in os.listdir(report_dir):
 		try:
 			main_report = parser.xml_to_str(report_dir+doc)
+			main_report = main_report
 			docs.append(main_report)
 		except:
 			pass
 
-	print(docs)
-		
-	#for doc in Tokenizer.pipe(docs):
-	#	print(doc)
-	#	for token in doc:
-	#		print(token.text, token.lemma_)
-
-
-	#rad_tokens = [token.text for token in nlp(doc) for doc in texts]
-	#print(rad_tokens)
-
-
+	return docs
+	
 
 def load_data(report_dir):
 	"""
 	Loads radiology reports into a 'Spacy' pipeline, including processing.
 	"""
 
-
 	#Load the English model
 	nlp = spacy.load("en_core_web_sm")
 
-	build_tokenizer(nlp, report_dir)
+	#Load up radiology reports into a list of strings
+	docs = build_tokenizable_data(nlp, report_dir)
+
+	#Begins first processing text data by tokens
+	for text in docs:
+		doc = nlp(text)
+		for token in doc:
+			print(token.text, token.lemma_, token.has_vector, token.vector_norm)
+
+	##NEXT STEP
 
 
 if __name__ == '__main__':
