b370e6e3869aaf2688ca34feea70f71115670fa4
sameersundrani <sundrani@stanford.edu>
2019-08-22 13:52:32 -0700

    new changes for python dictionary

--- spacy/spacy_ecgen_test1.py
+++ spacy/spacy_ecgen_test1.py
@@ -28,20 +28,23 @@ def build_tokenizable_data(nlp, report_dir):
 
     return docs
 
-
-# def helper(alltokens, token, strings, f, dicts):
+# not sure how to iterate functions but could be used in the future
+# def helper(token, keys, f, dicts):
+#     alltokens = {}
+#     alltokens[token.text] = []
 #     for i in range(len(dicts)):
-#         if strings[i] not in dicts[i]:
-#             dicts[i][strings[i]] = [token.f[i]]
+#         if keys[i] not in dicts[i]:
+#             dicts[i][keys[i]] = [token.f[i]]
 #         else:
-#             dicts[i][strings[i]].append(token.function[i])
+#             dicts[i][keys[i]].append(token.f[i])
 #         alltokens[token.text].append(dicts[i])
+#     return alltokens
 
-def build_doc_dicts(doc):
-    #Total dictionary of tokens first (if we need all tokens for example)
-    count = 1
+def build_doc_dicts(doc, report_dir):
+    #Total dictionary of tokens first (if we need all tokens for example), and counter for file name
     alltokens = {}
-    #strings for all dictionaries
+
+    #strings for all dictionaries, 7 so far
     HASVECTOR = str("has vector")
     VECTORNORM = str("vector norm")
     ISOOV = str("is oov")
@@ -49,9 +52,9 @@ def build_doc_dicts(doc):
     TAGS = str("tags")
     SHAPES = str("shapes")
     DEPS = str("deps")
-    LABELS = str("labels")
-    strings = [HASVECTOR, VECTORNORM, ISOOV, LEMMA, TAGS, SHAPES, DEPS]
-    functions = ["has_vector", "vector_norm", "is_oov", "lemma_", "tag_", "shape_", "dep_"]
+    # strings = [HASVECTOR, VECTORNORM, ISOOV, LEMMA, TAGS, SHAPES, DEPS]
+    # functions = [has_vector, vector_norm, is_oov, lemma, tag, shape, dep_]
+
     # multiple dictionaries for each feature to search for, dictionary in dictionary (Solar or Lucian indexing?)
     hasvector = {}
     vectornorm = {}
@@ -60,99 +63,89 @@ def build_doc_dicts(doc):
     tags = {}
     shapes = {}
     deps = {}
-    labels = {}
-    dicts = [hasvector, vectornorm, isoov, lemma, tags, shapes, deps]
-    for token in doc:
-        if token.text not in alltokens:
-            alltokens[token.text] = []
-            # helper(alltokens, token, strings, functions, dicts)
-            if HASVECTOR not in hasvector:
-                hasvector[HASVECTOR] = [token.has_vector]
+    # labels = {}
+    # dicts = [hasvector, vectornorm, isoov, lemma, tags, shapes, deps]
+    # iterature through tokens in a document and generate subdictionaries
+    for textfilename in os.listdir(report_dir):
+        for token in doc:
+            if token.text not in alltokens:
+                alltokens[token.text] = []
+                # alltokens.append(helper(alltokens, token, strings, functions, dicts))
+                if HASVECTOR not in hasvector:
+                    hasvector[HASVECTOR] = [token.has_vector]
+                else:
+                    hasvector[HASVECTOR].append(token.has_vector)
+                alltokens[token.text].append(hasvector)
+                if VECTORNORM not in vectornorm:
+                    vectornorm[VECTORNORM] = [token.vector_norm]
+                else:
+                    vectornorm[VECTORNORM].append(token.vector_norm)
+                alltokens[token.text].append(vectornorm)
+                if ISOOV not in isoov:
+                    isoov[ISOOV] = [token.is_oov]
+                else:
+                    isoov[ISOOV].append(token.is_oov)
+                alltokens[token.text].append(isoov)
+                if LEMMA not in lemma:
+                    lemma[LEMMA] = [token.lemma_]
+                else:
+                    lemma[LEMMA].append(token.lemma_)
+                alltokens[token.text].append(lemma)
+                if TAGS not in tags:
+                    tags[TAGS] = [token.tag_]
+                else:
+                    tags[TAGS].append(token.tag_)
+                alltokens[token.text].append(tags)
+                if SHAPES not in shapes:
+                    shapes[SHAPES] = [token.shape_]
+                else:
+                    shapes[SHAPES].append(token.shape_)
+                alltokens[token.text].append(shapes)
+                if DEPS not in deps:
+                    deps[DEPS] = [token.dep_]
+                else:
+                    deps[DEPS].append(token.dep_)
+                alltokens[token.text].append(deps)
             else:
-                hasvector[HASVECTOR].append(token.has_vector)
-            alltokens[token.text].append(hasvector)
-            if VECTORNORM not in vectornorm:
-                vectornorm[VECTORNORM] = [token.vector_norm]
-            else:
-                vectornorm[VECTORNORM].append(token.vector_norm)
-            alltokens[token.text].append(vectornorm)
-            if ISOOV not in isoov:
-                isoov[ISOOV] = [token.is_oov]
-            else:
-                isoov[ISOOV].append(token.is_oov)
-            alltokens[token.text].append(isoov)
-            if LEMMA not in lemma:
-                lemma[LEMMA] = [token.lemma_]
-            else:
-                lemma[LEMMA].append(token.lemma_)
-            alltokens[token.text].append(lemma)
-            if TAGS not in tags:
-                tags[TAGS] = [token.tag_]
-            else:
-                tags[TAGS].append(token.tag_)
-            alltokens[token.text].append(tags)
-            if SHAPES not in shapes:
-                shapes[SHAPES] = [token.shape_]
-            else:
-                shapes[SHAPES].append(token.shape_)
-            alltokens[token.text].append(shapes)
-            if DEPS not in deps:
-                deps[DEPS] = [token.dep_]
-            else:
-                deps[DEPS].append(token.dep_)
-            alltokens[token.text].append(deps)
-
-        else:
-            # helper(alltokens, token, strings, functions, dicts)
-            if HASVECTOR not in hasvector:
-                hasvector[HASVECTOR] = [token.has_vector]
-            else:
-                hasvector[HASVECTOR].append(token.has_vector)
-            alltokens[token.text].append(hasvector)
-            if VECTORNORM not in vectornorm:
-                vectornorm[VECTORNORM] = [token.vector_norm]
-            else:
-                vectornorm[VECTORNORM].append(token.vector_norm)
-            alltokens[token.text].append(vectornorm)
-            if ISOOV not in isoov:
-                isoov[ISOOV] = [token.is_oov]
-            else:
-                isoov[ISOOV].append(token.is_oov)
-            alltokens[token.text].append(isoov)
-            if LEMMA not in lemma:
-                lemma[LEMMA] = [token.lemma_]
-            else:
-                lemma[LEMMA].append(token.lemma_)
-            alltokens[token.text].append(lemma)
-            if TAGS not in tags:
-                tags[TAGS] = [token.tag_]
-            else:
-                tags[TAGS].append(token.tag_)
-            alltokens[token.text].append(tags)
-            if SHAPES not in shapes:
-                shapes[SHAPES] = [token.shape_]
-            else:
-                shapes[SHAPES].append(token.shape_)
-            alltokens[token.text].append(shapes)
-            if DEPS not in deps:
-                deps[DEPS] = [token.dep_]
-            else:
-                deps[DEPS].append(token.dep_)
-            alltokens[token.text].append(deps)
-        # if token.text not in isoov:
-        #     isoov[token.text] = [token.is_oov]
-        #
-        # else:
-        #     isoov[token.text].append(token.vector_norm)
-        #
-        # if token.text not in lemma:
-        #     lemma[token.text] = [token.lemma_]
-        # else:
-        #     lemma[token.text].append(token.lemma_)
-    f = open(str(count) + ".pkl", "wb")
-    pkl.dump(alltokens, f)
-    f.close()
-    count += 1
+                # alltokens.append(helper(alltokens, token, strings, functions, dicts))
+                if HASVECTOR not in hasvector:
+                    hasvector[HASVECTOR] = [token.has_vector]
+                else:
+                    hasvector[HASVECTOR].append(token.has_vector)
+                alltokens[token.text].append(hasvector)
+                if VECTORNORM not in vectornorm:
+                    vectornorm[VECTORNORM] = [token.vector_norm]
+                else:
+                    vectornorm[VECTORNORM].append(token.vector_norm)
+                alltokens[token.text].append(vectornorm)
+                if ISOOV not in isoov:
+                    isoov[ISOOV] = [token.is_oov]
+                else:
+                    isoov[ISOOV].append(token.is_oov)
+                alltokens[token.text].append(isoov)
+                if LEMMA not in lemma:
+                    lemma[LEMMA] = [token.lemma_]
+                else:
+                    lemma[LEMMA].append(token.lemma_)
+                alltokens[token.text].append(lemma)
+                if TAGS not in tags:
+                    tags[TAGS] = [token.tag_]
+                else:
+                    tags[TAGS].append(token.tag_)
+                alltokens[token.text].append(tags)
+                if SHAPES not in shapes:
+                    shapes[SHAPES] = [token.shape_]
+                else:
+                    shapes[SHAPES].append(token.shape_)
+                alltokens[token.text].append(shapes)
+                if DEPS not in deps:
+                    deps[DEPS] = [token.dep_]
+                else:
+                    deps[DEPS].append(token.dep_)
+                alltokens[token.text].append(deps)
+        f = open(str(textfilename) + ".pkl", "wb")
+        pkl.dump(alltokens, f)
+        f.close()
     # print(alltokens)
     # print(isoov)
     # print(lemma)
@@ -171,10 +164,10 @@ def load_data(report_dir):
     with open("out.pkl", "wb") as f:
         pkl.dump(list(docs), f)
 
-    #Begins first processing text data by tokenss
+    #Begins first processing text data by tokens
     for text in docs:
         doc = nlp(text)
-        build_doc_dicts(doc)
+        build_doc_dicts(doc, report_dir)
         break
 
     #my_model = nlp.to_disk("Users/ianwoodward/Documents/GitRepos/NLP-Radiolgy-/spacy/spacy_out")
@@ -190,8 +183,6 @@ def load_data(report_dir):
         #Named entity recognition
         #for ent in doc.ents:
         #	print(ent.text, ent.start_char, ent.end_char, ent.label_)
-
-
 
 if __name__ == '__main__':
     import sys
